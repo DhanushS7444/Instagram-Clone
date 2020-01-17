37a26ef3e97318c093ec1caf0ea54363a41e2ec7
Dhanush S <Dhanush@sharan.corp.capgemini.com>
2020-01-17 14:30:44 +0530

    End of first session

--- Instagram/ContentView.swift
+++ Instagram/ContentView.swift
@@ -13,7 +13,26 @@ struct ContentView: View {
     var body: some View {
         TabView{
             NavigationView{
-                Text("Home")
+                Home().navigationBarTitle("Instagram")
+                    .navigationBarItems(leading: Button(action: {
+                        
+                    }, label: {
+                        Image("Cam").resizable().frame(width: 30, height: 30)
+                    }).foregroundColor(Color("darkAndWhite"))
+                        ,trailing:
+                        HStack{
+                            Button(action: {
+                                
+                            }) {
+                                 Image("IGTV").resizable().frame(width: 30, height: 30)
+                            }.foregroundColor(Color("darkAndWhite"))
+                            Button(action: {
+                                
+                            }) {
+                                 Image("Send").resizable().frame(width: 30, height: 30)
+                            }.foregroundColor(Color("darkAndWhite"))
+                        }
+                )
             }.tabItem{
                 Image("Home")
             }
@@ -36,5 +55,11 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home : View {
+    var body : some View {
+         Text("Home")
     }
 }
